1db66802ca14fdff136b6780e355473bca6d3ce9
Miroslav Bajtoš <mbajtoss@gmail.com>
2019-08-26 10:35:07 +0200

    feat(booter-rest): initial commit
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

---
+++ packages/booter-rest/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: @loopback/booter-rest
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+// FIXME(bajtos) replace this dummy export with real implementation
+export const dummy = true;
